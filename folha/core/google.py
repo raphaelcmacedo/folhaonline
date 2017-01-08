3b3d857204e0897a9618cd42f90a63580093300d
raphaelcmacedo <raphaelcmacedo@hotmail.com>
2017-01-08 10:01:34 -0200

    Upload optimization

--- folha/core/google.py
+++ folha/core/google.py
@@ -4,9 +4,12 @@ import tempfile
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 
+drive = None
+
 def insert_file(contra_cheque, file):
 
-    drive = _auth()
+    if drive is None:
+        _auth()
 
     #Arquivo temporário
     fd, tmp = tempfile.mkstemp()
@@ -31,7 +34,8 @@ def insert_file(contra_cheque, file):
     contra_cheque.url = upload['alternateLink']
 
 def delete_file(contra_cheque):
-    drive = _auth()
+    if drive is None:
+        _auth()
     query = r"'root' in parents and trashed=false and name = '{}' ".format(get_file_name(contra_cheque))
     files = drive.ListFile({'q': query })
     for file in files:
@@ -39,6 +43,7 @@ def delete_file(contra_cheque):
 
 
 def _auth():
+
     gauth = GoogleAuth()
 
     gauth.LoadCredentialsFile("mycreds.txt")
@@ -54,7 +59,9 @@ def _auth():
     # Save the current credentials to a file
     gauth.SaveCredentialsFile("mycreds.txt")
 
-    return GoogleDrive(gauth)
+    global drive
+    drive = GoogleDrive(gauth)
+
 
 def get_file_name(contra_cheque):
     return '{}-{}-{}/{}.pdf'.format(contra_cheque.matricula.orgao.sigla, contra_cheque.matricula.numero, contra_cheque.mes, contra_cheque.exercicio)
